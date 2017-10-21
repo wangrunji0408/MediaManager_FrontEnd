d42740ef990b6bb22c30ecce0897b332442321e7
王润基 <wrj15@mails.tsinghua.edu.cn>
2017-10-21 16:54:52 +0800

    Signup: Add alert UI. #2

--- src/components/signup/index.ts
+++ src/components/signup/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import {UserApi} from '../../api';
 
 @Component({
   template: require('./signup.html')
@@ -12,10 +13,37 @@ export class Signup extends Vue {
   email: string = '';
   show_password: boolean = false;
 
-  signup () {
-    alert('signup');
-  }
   switch_show_password () {
     this.show_password = !this.show_password;
+  }
+
+  alert_type: string = '';
+  info: string = '';
+
+  alert_success (info: string) {
+    this.alert_type = 'success';
+    this.info = info;
+  }
+
+  alert_error (info: string) {
+    this.alert_type = 'danger';
+    this.info = info;
+  }
+
+  async signup () {
+    try {
+      let body = {
+        username: this.username,
+        password: this.password,
+        phone: this.phone,
+        email: this.email
+      };
+      let rsp = await new UserApi().signupUser(body);
+      this.alert_success(rsp.body);
+      window.location.href = '#/login';
+    } catch (e) {
+      this.alert_error(e.toString());
+      throw e;
+    }
   }
 }
