53aef00e52e63ae95db611ddd9452d719c70063e
王润基 <wrj15@mails.tsinghua.edu.cn>
2017-11-03 15:39:43 +0800

    SelfPage: Form validation & submit. #15

--- src/views/settings/selfpage/index.ts
+++ src/views/settings/selfpage/index.ts
@@ -36,13 +36,68 @@ export class SelfPage extends Vue {
   get passwordInputType() {
     return this.showPassword ? 'text' : 'password';
   }
+  get oldPasswordError() {
+    if (this.oldPassword1 !== this.oldPassword2)
+      return '密码不一致';
+    if (this.oldPassword1 === '')
+      return '';
+    return null;
+  }
+  get oldPasswordState() {
+    let error = this.oldPasswordError;
+    if (error === null)
+      return true;
+    if (error === '')
+      return 'null';
+    return false;
+  }
+  get newPasswordError() {
+    if (this.newPassword === '')
+      return '';
+    if (this.newPassword.length < 8)
+      return '密码长度必须>=8';
+    return null;
+  }
+  get newPasswordState() {
+    let error = this.newPasswordError;
+    if (error === null)
+      return true;
+    if (error === '')
+      return 'null';
+    return false;
+  }
 
   async onSubmit() {
-    this.$message.info('submit');
+    try {
+      await new UserApi().updateUser({id: this.user.id, body: this.user});
+      this.$message.success('修改用户信息成功');
+    } catch (e) {
+      this.$message.error('修改用户信息失败');
+    }
+  }
+
+  onPasswordModalShow() {
+    this.oldPassword1 = '';
+    this.oldPassword2 = '';
+    this.newPassword = '';
   }
 
   async onSubmitPassword() {
-    this.$message.info('submit_password');
+    if (!this.newPasswordState || !this.oldPasswordState) {
+      this.$message.error('信息错误');
+      return;
+    }
+    try {
+      await new UserApi().changeUserPassword({
+        id: this.user.id,
+        oldPassword: this.oldPassword1,
+        newPassword: this.newPassword
+      });
+      this.$message.success('修改密码成功');
+      (this.$refs.password_modal as any).hide();
+    } catch (e) {
+      this.$message.error('修改密码失败');
+    }
   }
 
   async fetchData () {
